75d47e79dea7e23e32781022a20d5e45802bc116
maroofiums <maroof96965@gmail.com>
2026-08-19 14:07:58 +0500

    define application schemas

--- job-application-tracker/applications/admin.py
+++ job-application-tracker/applications/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Application, Company
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "industry",
+        "location",
+        "created_at"
+    )
+
+    search_fields = (
+        "name",
+        "industry",
+        "location",
+    )
+
+@admin.register(Application)
+class ApplicationAdmin(admin.ModelAdmin):
+    list_display = (
+        "position",
+        "company",
+        "user",
+        "status",
+        "employment_type",
+        "applied_at"
+    )
+
+    list_filter = (
+        "status",
+        "employment_type",
+        "applied_at"
+    )
+
+    search_fields = (
+        "position",
+        "company__name",
+        "user__username"
+    )
+
+    autocomplete_fields = (
+        "company",
+        "user"
+    )
